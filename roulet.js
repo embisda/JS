58f54d2201ee3d73bdb5fc6b36f313d18cb2ce61
EMBISDA <usfame@gmail.com>
2017-03-13 22:05:37 +0300

    Update roulet.js

--- roulet.js
+++ roulet.js
@@ -4,6 +4,8 @@ var red = 0;
 var black = 0;
 var arr = [];
 var i = 0;
+var length = 0;
+var max_length = 0;
 for (i=0; i<=rounds; i++) {
 	var j = (Math.round(Math.random() * 36));
   if (j == 0) {
@@ -29,3 +31,16 @@ for (i=0; i<arr.length; i++) {
   }
 }
 alert('Частота выпадения красного: ' + Math.round(red)/arr.length*100 + '%. Частота выпадения черного: ' + Math.round(black)/arr.length*100 + '%. Частота выпадени zero: ' + Math.round(zero)/arr.length*100 + '%.');
+for (i=0; i<arr.length; i++) {
+	if(arr[i] == 1) {
+  	length = 0;
+    while(i<arr.length && arr[i] == 1) {
+    	i++;
+      length++;
+    }
+    if (length > max_length) {
+    	max_length = length
+    }
+  }
+}
+alert('Максимальная длинна чёрной последовательности: ' + max_length);
